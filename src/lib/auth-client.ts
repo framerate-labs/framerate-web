4f32ce353edb28d448fcf7fb077cca6e518d0b5e
Michael Mkrtchyan <michaelmkrtch@gmail.com>
2025-10-17 15:07:41 -0700

    fix: update Better-Auth base url

--- src/lib/auth-client.ts
+++ src/lib/auth-client.ts
@@ -2,9 +2,8 @@ import { usernameClient } from 'better-auth/client/plugins';
 import { createAuthClient } from 'better-auth/svelte';
 
 import { dev } from '$app/environment';
-import { PUBLIC_AUTH_ORIGIN } from '$env/static/public';
 
-const baseURL = PUBLIC_AUTH_ORIGIN || (dev ? 'http://localhost:8000' : 'https://api.frame-rate.io');
+const baseURL = dev ? 'http://localhost:8000' : 'https://api.frame-rate.io';
 
 export const authClient = createAuthClient({
 	baseURL,
